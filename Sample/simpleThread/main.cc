effbec68c91de8c88195970b3c2e614f0d732570
lincleejun <419149995@qq.com>
2012-03-11 20:27:53 +0800

    modified simplethread  add a thread pool sample

--- Sample/simpleThread/main.cc
+++ Sample/simpleThread/main.cc
@@ -5,19 +5,38 @@ using namespace std;
 class MyDelegate : public base::DelegateSimpleThread::Delegate
 {
 public:
+  void SetVal(int n)
+  {
+    m_val = n;
+  }
   void Run()
   {
-    cout << "In MyDelegate Thread Run" << endl;
+    cout << "In MyDelegate Thread Run : " << m_val << endl;
     base::PlatformThread::Sleep(1000);
   }
+private:
+  int m_val;
 };
 
 
 int main()
 {
+#if 0
   MyDelegate myDelegate;
   base::DelegateSimpleThread thread(&myDelegate,"test Thread");
   thread.Start();
   thread.Join();
+#else
+  base::DelegateSimpleThreadPool threadPool("ahaha",5);
+  MyDelegate myDelegate[10];
+  threadPool.Start();
+  for (int i=0; i<10; ++i)
+  {
+    myDelegate[i].SetVal(i+1);
+    threadPool.AddWork(&myDelegate[i]);
+  }
+  threadPool.JoinAll();
+
+#endif
   return 0;
 }
